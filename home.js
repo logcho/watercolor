6751e1eb4b2adc2a9741dce9c1c8b1f72bf10335
logcho <108314617+logcho@users.noreply.github.com>
2023-06-14 00:21:49 -0500

    Update home.js

--- home.js
+++ home.js
@@ -1,42 +1,32 @@
-window.onload = function() {
-  
-    // Rest of the code here
-  
-    var countDownDate = new Date("July 1, 2023 00:00:00").getTime();
-  
-    // Update the count down every 1 second
-    var x = setInterval(function() {
-      // Get today's date and time
-      var now = new Date().getTime();
-  
-      // Find the distance between now and the count down date
-      var distance = countDownDate - now;
-  
-      // Time calculations for days, hours, minutes and seconds
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-      // Add leading zeros for tens digits less than 10
-      hours = hours < 10 ? '0' + hours : hours;
-      minutes = minutes < 10 ? '0' + minutes : minutes;
-      seconds = seconds < 10 ? '0' + seconds : seconds;
-  
-      var dayEl = document.getElementById('days');
-      dayEl.innerHTML = days;
-      var hoursEl = document.getElementById('hours');
-      hoursEl.innerHTML = hours;
-      var minutesEl = document.getElementById('minutes');
-      minutesEl.innerHTML = minutes;
-      var secondsEl = document.getElementById('seconds');
-      secondsEl.innerHTML = seconds;
-  
-      // If the count down is over, write some text
-      if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
-      }
-    }, 1000);
-  }
-  
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+  // Set the release date
+  let releaseDate = new Date("06/14/2023 00:20:00"); // Change the release date and time here
+
+  const countDown = releaseDate.getTime(),
+        x = setInterval(function() {    
+          const now = new Date().getTime(),
+                distance = countDown - now;
+
+
+          document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+
+          // Do something later when the date is reached
+          if (distance < 0) {
+            document.getElementById("days").innerText = "0",
+            document.getElementById("hours").innerText = "0",
+            document.getElementById("minutes").innerText = "0",
+            document.getElementById("seconds").innerText = "0";
+            window.location.href = "release.html";
+            clearInterval(x);
+          }
+        }, 0);
+})();
