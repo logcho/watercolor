4b76b72e4b17baca7552488d5d5708718c5edb07
logcho <108314617+logcho@users.noreply.github.com>
2023-07-02 17:38:20 -0500

    Add files via upload

---
+++ release.js
@@ -0,0 +1,26 @@
+function createCircle() {
+    const circle = document.createElement('div');
+    circle.classList.add('circle');
+  
+    const size = Math.random() * 100 + 50; // Random size between 10 and 100 pixels
+    const color = `hsl(${Math.random() * 360}, 70%, 50%)`; // Random color using HSL
+  
+    circle.style.width = `${size}px`;
+    circle.style.height = `${size}px`;
+    circle.style.background = color;
+  
+    const x = Math.random() * window.innerWidth; // Random x-coordinate within the window width
+    const y = Math.random() * window.innerHeight; // Random y-coordinate within the window height
+  
+    circle.style.left = `${x}px`;
+    circle.style.top = `${y}px`;
+  
+    document.getElementById('background').appendChild(circle);
+  
+    setTimeout(() => {
+      circle.remove();
+    }, 2000); // Remove the circle after 2 seconds
+  }
+  
+  setInterval(createCircle, 500); // Create a circle every 0.5 seconds
+  
