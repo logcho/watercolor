3119bb2f267a52e81e19f3bbd93b839d4e39cc22
logcho <108314617+logcho@users.noreply.github.com>
2023-07-02 17:53:05 -0500

    Add files via upload

---
+++ text.js
@@ -0,0 +1,44 @@
+window.onload = () => {
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+  
+    let intervals = [];
+  
+    const h2Elements = document.querySelectorAll("h2");
+  
+    const applyEffect = (element) => {
+      let interval = null;
+      let iteration = 0;
+  
+      clearInterval(interval);
+  
+      interval = setInterval(() => {
+        element.innerText = element.innerText
+          .split("")
+          .map((letter, index) => {
+            if (index < iteration) {
+              return element.dataset.value[index].toLowerCase();
+            }
+  
+            return letters[Math.floor(Math.random() * 26)];
+          })
+          .join("");
+  
+        if (iteration >= element.dataset.value.length) {
+          clearInterval(interval);
+        }
+  
+        iteration += 1 / 10;
+      }, 30);
+  
+      intervals.push(interval);
+    };
+  
+    // Apply effect on mouseover for each h1 element
+    h2Elements.forEach((element) => {
+      element.onmouseover = () => applyEffect(element);
+    });
+  
+    // Apply effect onload for each h1 element
+    h2Elements.forEach(applyEffect);
+  };
+  
