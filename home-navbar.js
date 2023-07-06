ca0d4c7580127c1475fe68ae676dc33631ee4fb7
logcho <108314617+logcho@users.noreply.github.com>
2023-07-05 20:38:26 -0500

    Update home-navbar.js

--- home-navbar.js
+++ home-navbar.js
@@ -1,26 +1,34 @@
-
 const toggleBtn = document.querySelector('.toggle_btn');
 const dropDownMenu = document.querySelector('.dropdown_menu');
 const overlay = document.querySelector('.overlay');
-const popup = document.querySelector('.popup')
+const popup = document.querySelector('.popup');
+
+let isFirstClick = true; // Flag variable to track the first click
 
 toggleBtn.onclick = function() {
   dropDownMenu.classList.toggle('open');
   toggleBtn.classList.toggle('open');
   overlay.classList.toggle('open');
-  
+
   // Add shading effect
   if (dropDownMenu.classList.contains('open')) {
     overlay.style.display = 'block'; // Show the overlay when the dropdown menu is open
   } else {
     overlay.style.display = 'none'; // Hide the overlay when the dropdown menu is closed
   }
+
+  // Toggle popup only on the first click
+  if (isFirstClick) {
+    popup.classList.toggle('open');
+    isFirstClick = false; // Set the flag to false after the first click
+  }
 };
+
 overlay.onclick = function() {
   dropDownMenu.classList.toggle('open');
   toggleBtn.classList.toggle('open');
   overlay.classList.toggle('open');
-  
+
   // Add shading effect
   if (dropDownMenu.classList.contains('open')) {
     overlay.style.display = 'block'; // Show the overlay when the dropdown menu is open
@@ -28,10 +36,3 @@ overlay.onclick = function() {
     overlay.style.display = 'none'; // Hide the overlay when the dropdown menu is closed
   }
 };
-
-popup.onclick = function() {
-  dropDownMenu.classList.toggle('open');
-  toggleBtn.classList.toggle('open');
-  overlay.classList.toggle('open');
-  popup.classList.toggle('open');
-}
