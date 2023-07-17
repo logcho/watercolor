514ef893df7295430fe42394e9eefa1ac03de4ff
logcho <108314617+logcho@users.noreply.github.com>
2023-07-17 15:29:11 -0500

    Update release.js

--- release.js
+++ release.js
@@ -5,15 +5,17 @@ function createCircle() {
   const size = Math.random() * 100 + 50; // Random size between 50 and 150 pixels
   const color = `hsl(${Math.random() * 360}, 70%, 50%)`; // Random color using HSL
 
-  // Ensure the circle is completely within the visible area
-  const radius = size / 2;
-  const maxX = window.innerWidth - size;
-  const maxY = window.innerHeight - size;
-  const minX = radius;
-  const minY = radius;
+  const padding = 20; // Padding in pixels
 
-  const x = Math.random() * (maxX - minX) + minX; // Random x-coordinate within the visible width
-  const y = Math.random() * (maxY - minY) + minY; // Random y-coordinate within the visible height
+  // Ensure the circle is completely within the visible area with padding
+  const radius = size / 2;
+  const maxX = window.innerWidth - size - padding * 2;
+  const maxY = window.innerHeight - size - padding * 2;
+  const minX = radius + padding;
+  const minY = radius + padding;
+
+  const x = Math.random() * (maxX - minX) + minX; // Random x-coordinate within the visible width with padding
+  const y = Math.random() * (maxY - minY) + minY; // Random y-coordinate within the visible height with padding
 
   circle.style.width = `${size}px`;
   circle.style.height = `${size}px`;
